6dc38a09d728114978bc5a512cb3ff4cdb5336f5
Tsiliii <32791023+Tsiliii@users.noreply.github.com>
2019-12-31 01:18:51 +0200

    Add files via upload

--- local/dict/lexicon2.py
+++ local/dict/lexicon2.py
@@ -1,8 +1,8 @@
 from string import maketrans
 
 def main():
-	w = open("nonsilence_phones.txt.txt","w+")
-	w2 = open("lexicon2.txt.txt","w+")
+	w = open("nonsilence_phones.txt","w+")
+	w2 = open("lexicon2.txt","w+")
 	r = open("uttids.txt","r")
 	trans = open("transcription.txt","r")
 	lex = open("lexicon.txt","r")
